e88c2e90b80ee812fb466417ed8dcad57a4f1401
NAYEEM <nayeem0772@gmail.com>
2022-11-09 13:53:40 +0530

    Added Jenkins File

--- vars/common.groovy
+++ vars/common.groovy
@@ -4,13 +4,37 @@ def codeQuality()
             echo 'Code Quality'
     }
 }
-def styleChecks()
-{
+
+def codeChecks()    {
     if ( BRANCH_NAME == "main" || TAG_NAME ==~ ".*" ){
+
         stage('Style Checks') {
             echo 'Style Checks'
+        }
 
+        stage('unit tests') {
+            echo 'unit tests'
         }
     }
+}
+
+def artifacts()     {
+    if ( TAG_NAME ==~ ".*" ) {
+
+        stage('Download Dependencies')  {
+                echo 'Download Dependencies'
+        }
+
+        stage('Prepare Artifact') {
+                echo 'Prepare Artifact'
+        }
+
+        stage('Publish Artifact') {
+                echo 'Publish Artifact'
+        }
+
+
+    }
+
 }
 
--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -7,38 +7,13 @@ def call()  {
             env.TAG_NAME = ""
         }
             common.codeQuality()
-            common.styleChecks()
+            common.codeChecks()
+            common.artifacts()
 
 
-//            stage('Unit Tests') {
-//                when
-//                        {
-//                            anyOf
-//                                    {
-//                                        branch 'main'
-//                                        tag "*"
-//                                    }
-//                        }
-//                steps {
-//                    echo 'Code Quality'
-//                }
-//            }
-//
 
-//            stage('Prepare Artifact') {
-//                when { tag "*"}
-//                steps {
-//                    echo 'Prepare Artifact'
-//                }
-//            }
-//
-//            stage('Publish Artifact') {
-//                when { tag "*"}
-//                steps {
-//                    echo 'Publish Artifact'
-//                }
-//            }
-//
+
+
 //
 //
 //        }
