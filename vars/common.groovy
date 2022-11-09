4770a22868aa38ea1f30f76462ab619264dbde53
NAYEEM <nayeem0772@gmail.com>
2022-11-09 23:58:30 +0530

    Added Jenkins File

--- vars/common.groovy
+++ vars/common.groovy
@@ -3,7 +3,7 @@ def codeQuality()
     stage('Code Quality') {
         withCredentials([usernamePassword(credentialsId: 'SONAR', passwordVariable: 'sonarPass', usernameVariable: 'sonarUser')]) {
             sh '''
-            sonar-scanner -D sonar.host.url=http://172.31.6.176:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=$(COMPONENT)
+                 sonar-scanner -D sonar.host.url=http://172.31.6.176:9000 -Dsonar.login=$(sonarUser) -Dsonar.password=$(sonarPass) -Dsonar.projectKey=$(COMPONENT)
         '''
         }
     }
