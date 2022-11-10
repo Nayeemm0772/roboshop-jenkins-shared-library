6b27854037cc5eab0ff3ccdd4f2d4ec7ac6b853f
NAYEEM <nayeem0772@gmail.com>
2022-11-10 23:47:42 +0530

    Added Jenkins File

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,11 +1,15 @@
 def call()  {
 
     node {
-
-
+        try {
             common.codeQuality()
             common.codeChecks()
             common.artifacts()
+        } catch (Exception e) {
+            mail bcc: '', body: 'build failed ${RUN_DISPLAY_URL}', cc: '', from: 'nayeem0772@gmail.com', replyTo: '', subject: 'BUILD FAILURE', to: 'nayeem0772@gmail.com'
+        }
+
+
 
 
 
